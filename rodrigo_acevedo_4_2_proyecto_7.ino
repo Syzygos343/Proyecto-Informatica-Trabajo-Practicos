d5580eeac75a30daea4f5802e20b8932858f6d3b
Rodrigo Acevedo <rodriacev08@gmail.com>
2024-12-10 08:54:07 -0300

    Add files via upload

---
+++ rodrigo_acevedo_4_2_proyecto_7.ino
@@ -0,0 +1,212 @@
+
+void song(int buzzerPin){
+  
+  tone(buzzerPin, 392);
+  delay(273);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(273);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 494);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 494);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 494);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 370);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 330);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 466);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 440);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(136);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 392);
+  delay(409);
+  noTone(buzzerPin);
+}
+
+void setup() {
+  pinMode(8, OUTPUT);
+}
+
+void loop() {
+  song(8);
+  delay(1000);
+}
